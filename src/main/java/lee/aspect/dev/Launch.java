44774ff0bb3354c6f0c903353c75852802956068
Aspect99 <lee178lee@gmail.com>
2022-10-19 23:04:07 -0700

    add check to see if muti instance of application is launched

--- src/main/java/lee/aspect/dev/Launch.java
+++ src/main/java/lee/aspect/dev/Launch.java
@@ -1,8 +1,23 @@
 package lee.aspect.dev;
 
 import lee.aspect.dev.application.CustomDiscordRPC;
+import lee.aspect.dev.application.RunLoopManager;
+import lee.aspect.dev.jsonreader.FileManager;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 public class Launch {
+
+    public static File f;
+    public static FileChannel channel;
+    public static FileLock lock;
+
+
     /**
      * Redirect main Class to {@link CustomDiscordRPC Launch}
      * This program is used to customize Discord rich perference via a interface by JavaFX
@@ -11,6 +26,45 @@ public class Launch {
      * @author Aspect
      */
     public static void main(String[]args){
-        CustomDiscordRPC.Launch(args);
+        try {
+            f = new File(FileManager.getROOT_DIR(),"runtime");
+            if(f.exists()) f.delete();
+            channel = new RandomAccessFile(f,"rw").getChannel();
+            lock = channel.tryLock();
+
+            if(lock == null) {
+                channel.close();
+                String message = "\"Custom Discord RP\"\n"
+                        + "It looks like you are trying to create\n"
+                        + "mutiple instance of the program";
+                JOptionPane.showMessageDialog(new JFrame(), message, "Application Running",
+                        JOptionPane.ERROR_MESSAGE);
+                System.exit(-1);
+            }
+
+            Thread shutdown = new Thread(() -> {
+                unlock();
+            });
+
+            Runtime.getRuntime().addShutdownHook(shutdown);
+            CustomDiscordRPC.Launch(args);
+        } catch (IOException e){
+            throw new RuntimeException("Could not start application",e);
+        }
+
     }
+
+    public static void unlock() {
+        try {
+            if(lock != null) {
+                lock.release();
+                channel.close();
+                f.delete();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
