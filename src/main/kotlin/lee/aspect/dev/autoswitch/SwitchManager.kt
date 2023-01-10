1e23ee09b824ee24f4dd7407852e8cf9096f997a
lee <108044327+nullpointerexceptionkek@users.noreply.github.com>
2023-01-10 13:01:20 -0800

    made the menu

--- src/main/kotlin/lee/aspect/dev/autoswitch/SwitchManager.kt
+++ src/main/kotlin/lee/aspect/dev/autoswitch/SwitchManager.kt
@@ -25,19 +25,54 @@
 
 package lee.aspect.dev.autoswitch
 
+import javafx.geometry.Insets
 import javafx.scene.Parent
-import javafx.scene.layout.AnchorPane
+import javafx.scene.control.ScrollPane
+import javafx.scene.control.TextField
+import javafx.scene.layout.HBox
 import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
+import javafx.scene.text.Text
+import lee.aspect.dev.config.ConfigManager
+
 
 abstract class SwitchManager {
 
     companion object {
         @JvmStatic
         fun initMenu(): Parent {
+            val fileNames = ConfigManager.getCurrentConfigFiles()
+            val vbox = VBox()
+
+            if (fileNames != null) {
+                for (file in fileNames) {
+                    val hbox = HBox()
+                    val text = Text(file.name)
+                    val textField = TextField()
+                    hbox.children.addAll(text, textField)
+                    vbox.children.add(hbox)
+                }
+            }
+
+            val scrollPane = ScrollPane()
+            scrollPane.content = vbox
+
             val stackPane = StackPane()
-            val anchorRoot = AnchorPane()
-            anchorRoot.setPrefSize(540.0, 334.0)
-            stackPane.children.add(anchorRoot)
+            stackPane.padding = Insets(10.0)
+            stackPane.setPrefSize(334.0,540.0)
+
+            if (fileNames!!.size > stackPane.height / 30) {
+                // Show the scrollbar
+                scrollPane.isFitToWidth = true
+                stackPane.children.add(scrollPane)
+            } else {
+                // Don't show the scrollbar
+                stackPane.children.add(vbox)
+            }
+
+
+            println(stackPane.prefHeight)
+
             return stackPane
         }
     }
