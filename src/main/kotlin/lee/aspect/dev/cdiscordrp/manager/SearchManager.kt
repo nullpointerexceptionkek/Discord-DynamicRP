a7e5ca1a0a4f0d4b1c42c37384f9bec53de96893
Aspect99 <lee178lee@gmail.com>
2023-05-27 14:51:32 -0700

    fixed css for searchamanger

--- src/main/kotlin/lee/aspect/dev/cdiscordrp/manager/SearchManager.kt
+++ src/main/kotlin/lee/aspect/dev/cdiscordrp/manager/SearchManager.kt
@@ -27,9 +27,14 @@ package lee.aspect.dev.cdiscordrp.manager
 
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
+import javafx.geometry.Insets
 import javafx.scene.control.*
 import javafx.scene.control.cell.PropertyValueFactory
+import javafx.scene.image.Image
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
+import javafx.stage.Stage
 import lee.aspect.dev.cdiscordrp.application.controller.EditListController
 import lee.aspect.dev.cdiscordrp.application.core.Script
 import lee.aspect.dev.cdiscordrp.application.core.Settings
@@ -37,9 +42,8 @@ import lee.aspect.dev.cdiscordrp.application.core.Updates
 import java.util.*
 
 class SearchManager {
-
     companion object {
-        private val searchMode = listOf(
+        private val searchModes = listOf(
             "All",
             "First Line",
             "Second Line",
@@ -56,185 +60,128 @@ class SearchManager {
 
         @JvmStatic
         fun showDialog() {
-            val dialog = Dialog<String>()
-            dialog.title = "Search"
-            dialog.headerText = "Find an item in the list"
+            val dialog = createDialog()
+            val textInput = createTextInput()
+            val searchChoiceBox = createSearchChoiceBox()
+            val tableView = createTableView()
 
-            val textInput = TextField()
-
-            val searchChoiceBox = ChoiceBox(FXCollections.observableList(searchMode))
-            searchChoiceBox.selectionModel.select(0)
-
-            val tableView = TableView<Updates>()
-            val firstLineColumn = TableColumn<Updates, String>("First Line")
-            firstLineColumn.cellValueFactory = PropertyValueFactory("fl")
-
-            val secondLineColumn = TableColumn<Updates, String>("Second Line")
-            secondLineColumn.cellValueFactory = PropertyValueFactory("sl")
-
-            val delayColumn = TableColumn<Updates, Long>("Delay")
-            delayColumn.cellValueFactory = PropertyValueFactory("wait")
-
-            val largeImgColumn = TableColumn<Updates, String>("Large Img")
-            largeImgColumn.cellValueFactory = PropertyValueFactory("image")
-
-            val largeImgTextColumn = TableColumn<Updates, String>("Large Img Text")
-            largeImgTextColumn.cellValueFactory = PropertyValueFactory("imagetext")
-
-            val smallImgColumn = TableColumn<Updates, String>("Small Img")
-            smallImgColumn.cellValueFactory = PropertyValueFactory("smallimage")
-
-            val smallImgTextColumn = TableColumn<Updates, String>("Small Img Text")
-            smallImgTextColumn.cellValueFactory = PropertyValueFactory("smalltext")
-
-            val button1Column = TableColumn<Updates, String>("Button 1")
-            button1Column.cellValueFactory = PropertyValueFactory("button1Text")
-
-            val button1URLColumn = TableColumn<Updates, String>("Button 1 URL")
-            button1URLColumn.cellValueFactory = PropertyValueFactory("button1Url")
-
-            val button2Column = TableColumn<Updates, String>("Button 2")
-            button2Column.cellValueFactory = PropertyValueFactory("button2Text")
-
-            val button2URLColumn = TableColumn<Updates, String>("Button 2 URL")
-            button2URLColumn.cellValueFactory = PropertyValueFactory("button2Url")
-
-            tableView.columns.addAll(
-                firstLineColumn,
-                secondLineColumn,
-                delayColumn,
-                largeImgColumn,
-                largeImgTextColumn,
-                smallImgColumn,
-                smallImgTextColumn,
-                button1Column,
-                button1URLColumn,
-                button2Column,
-                button2URLColumn
-            )
-
-            val originalItems: List<Updates> = Script.getINSTANCE().totalupdates
-            val items: ObservableList<Updates> = FXCollections.observableList(originalItems.toMutableList())
-            tableView.setRowFactory {
-                val row = TableRow<Updates>()
-                row.setOnMouseClicked { event ->
-                    if (event.clickCount == 2 && !row.isEmpty) {
-                        EditListController.showListConfig(originalItems.indexOf(row.item),dialog.x,dialog.y)
-                    }
-                }
-                row
+            val inputBox = HBox(10.0, textInput, searchChoiceBox).apply {
+                HBox.setHgrow(textInput, Priority.ALWAYS)
             }
-            tableView.items = items
-
-            val content = VBox()
-            content.children.addAll(textInput, searchChoiceBox, tableView)
-
-            val okButton = ButtonType.OK
-            dialog.dialogPane.buttonTypes.addAll(okButton)
+            val content = VBox(10.0, inputBox, tableView)
 
             dialog.dialogPane.content = content
+            setupDialogResultConverter(dialog)
+            setupTextInputListener(textInput, searchChoiceBox, tableView)
 
-            dialog.setResultConverter { buttonType ->
-                if (buttonType == okButton) {
-                    dialog.close()
-                }
-                null
-            }
-            textInput.textProperty().addListener { _, _, newValue ->
-                val searchText = newValue.trim()
-                if (searchText.isNotEmpty()) {
-                    when (searchChoiceBox.selectionModel.selectedItem) {
-                        "All" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item -> item.matches(searchText) }
-                        }
-
-                        "First Line" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.fl.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Second Line" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.sl.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Delay" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.wait.toString().lowercase(Locale.ROOT)
-                                    .contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Large Img" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.image.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Large Img Text" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.imagetext.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Small Img" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.smallimage.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Small Img Text" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.smalltext.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Button 1" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.button1Text.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Button 1 URL" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.button1Url.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Button 2" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.button2Text.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-
-                        "Button 2 URL" -> {
-                            tableView.selectionModel.clearSelection()
-                            tableView.items = items.filtered { item ->
-                                item.button2Url.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT))
-                            }
-                        }
-                    }
-                } else {
-                    tableView.selectionModel.clearSelection()
-                    tableView.items = items
-                }
-            }
             dialog.dialogPane.stylesheets.add(Settings.getINSTANCE().theme.path)
             dialog.show()
         }
 
+        private fun createDialog(): Dialog<String> {
+            val dialog = Dialog<String>()
+            dialog.title = "Search"
+            dialog.headerText = null
+            dialog.dialogPane.buttonTypes.addAll(ButtonType.OK)
+            val stage = (dialog.dialogPane.scene.window as Stage)
+            dialog.isResizable = true
+            stage.icons.add(Image(Objects.requireNonNull(Companion::class.java.getResourceAsStream("/lee/aspect/dev/cdiscordrp/icon/search.png"))))
+            return dialog
+        }
+
+        private fun createTextInput() = TextField()
+
+        private fun createSearchChoiceBox(): ChoiceBox<String> {
+            val searchChoiceBox = ChoiceBox(FXCollections.observableList(searchModes))
+            searchChoiceBox.selectionModel.select(0)
+            return searchChoiceBox
+        }
+
+        private fun createTableView(): TableView<Updates> {
+            val tableView = TableView<Updates>()
+            tableView.columns.addAll(createColumns())
+            tableView.setRowFactory { createRowFactory() }
+            tableView.items = getInitialItems()
+            return tableView
+        }
+
+        private fun createColumns(): List<TableColumn<Updates, *>> = listOf(
+            createColumn("First Line", "fl"),
+            createColumn("Second Line", "sl"),
+            createColumn("Delay", "wait"),
+            createColumn("Large Img", "image"),
+            createColumn("Large Img Text", "imagetext"),
+            createColumn("Small Img", "smallimage"),
+            createColumn("Small Img Text", "smalltext"),
+            createColumn("Button 1", "button1Text"),
+            createColumn("Button 1 URL", "button1Url"),
+            createColumn("Button 2", "button2Text"),
+            createColumn("Button 2 URL", "button2Url")
+        )
+
+        private fun createColumn(title: String, property: String): TableColumn<Updates, *> {
+            val column = TableColumn<Updates, Any>(title)
+            column.cellValueFactory = PropertyValueFactory(property)
+            return column
+        }
+
+        private fun createRowFactory(): TableRow<Updates> {
+            val row = TableRow<Updates>()
+            val dialog = createDialog()
+            row.setOnMouseClicked { event ->
+                if (event.clickCount == 2 && !row.isEmpty) {
+                    val originalItems = getInitialItems()
+                    EditListController.showListConfig(originalItems.indexOf(row.item), dialog.x, dialog.y)
+                }
+            }
+            return row
+        }
+
+        private fun getInitialItems(): ObservableList<Updates> {
+            val originalItems: List<Updates> = Script.getINSTANCE().totalupdates
+            return FXCollections.observableList(originalItems.toMutableList())
+        }
+
+        private fun setupDialogResultConverter(dialog: Dialog<String>) {
+            dialog.setResultConverter { buttonType ->
+                if (buttonType == ButtonType.OK) {
+                    dialog.close()
+                }
+                null
+            }
+        }
+
+        private fun setupTextInputListener(textInput: TextField, searchChoiceBox: ChoiceBox<String>, tableView: TableView<Updates>) {
+            textInput.textProperty().addListener { _, _, newValue ->
+                val searchText = newValue.trim()
+                if (searchText.isNotEmpty()) {
+                    tableView.selectionModel.clearSelection()
+                    val filterFunction = getFilterFunction(searchChoiceBox, searchText)
+                    tableView.items = getInitialItems().filtered(filterFunction)
+                } else {
+                    tableView.selectionModel.clearSelection()
+                    tableView.items = getInitialItems()
+                }
+            }
+        }
+
+        private fun getFilterFunction(searchChoiceBox: ChoiceBox<String>, searchText: String): (Updates) -> Boolean {
+            return when (searchChoiceBox.selectionModel.selectedItem) {
+                "All" -> { item -> item.matches(searchText) }
+                "First Line" -> { item -> item.fl.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Second Line" -> { item -> item.sl.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Delay" -> { item -> item.wait.toString().lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Large Img" -> { item -> item.image.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Large Img Text" -> { item -> item.imagetext.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Small Img" -> { item -> item.smallimage.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Small Img Text" -> { item -> item.smalltext.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Button 1" -> { item -> item.button1Text.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Button 1 URL" -> { item -> item.button1Url.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Button 2" -> { item -> item.button2Text.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                "Button 2 URL" -> { item -> item.button2Url.lowercase(Locale.ROOT).contains(searchText.lowercase(Locale.ROOT)) }
+                else -> { _ -> false }
+            }
+        }
     }
+
 }
